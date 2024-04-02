67e27006b4c27754e9375519268fb5e04c943c91
Rune H. Steinnes <rune.steinnes@westum.no>
2024-04-02 20:39:50 +0200

    Moved from pyright to pylsp

--- README.md
+++ README.md
@@ -4,6 +4,34 @@
 
 A template for getting started with [AstroNvim](https://github.com/AstroNvim/AstroNvim)
 
+# Note to self.
+
+nvim/lua/plugins/mason.lua
+
+moved from "pyright", "ruff-lsp" to "pyls"
+
+For this to work :Pylspinstall pylsp-ruff
+
+The [tool.ruff] section in pyproject.toml is honored!
+
+I have not been able to make the server section in mason.lua work.
+
+```json
+pylsp ={
+  settings = {
+    pylsp = {
+    plugins = {
+    },
+  },
+}
+```
+
+I've tried everything.
+
+Also, install mypy.
+
+Will try this config for a bit.. to see if I like it better than pyright.
+
 ## 🛠️ Installation
 
 #### Make a backup of your current nvim and shared folder
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,52 +2,71 @@
 
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
-  {
-    "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
-        "pyright",
-        "ruff_lsp",
-        server = {
-          pyright = {
-            autoImportCompletion = true,
-          },
-          ruff_lsp = {
-            resolved_capabilities = {
-              hover = false,
-            },
-          },
-        },
-        -- add more arguments for adding more language servers
-      })
-    end,
-  },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
-  {
-    "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "prettier",
-        "stylua",
-        -- add more arguments for adding more null-ls sources
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
-    opts = function(_, opts)
-      -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "python",
-        -- add more arguments for adding more debuggers
-      })
-    end,
-  },
+	-- use mason-lspconfig to configure LSP installations
+	{
+		"williamboman/mason-lspconfig.nvim",
+		-- overrides `require("mason-lspconfig").setup(...)`
+		opts = function(_, opts)
+			-- add more things to the ensure_installed table protecting against community packs modifying it
+			opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+				"lua_ls",
+				"pylsp",
+				server = {
+					pylsp = {
+						plugins = {
+							ruff = {
+								extendSelect = { "A", "B", "C4", "D", "I", "ERA", "PLR", "SIM", "TCH" },
+								ignore = {
+									"I001",
+									"D101",
+									"D102",
+									"D103",
+									"D107",
+									"D202",
+									"D400",
+									"D401",
+									"D406",
+									"D407",
+									"D412",
+									"D417",
+								},
+								showFixes = true,
+								lineLength = 119,
+							},
+						},
+					},
+					ruff_lsp = {
+						resolved_capabilities = {
+							hover = false,
+						},
+					},
+				},
+				-- add more arguments for adding more language servers
+			})
+		end,
+	},
+	-- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+	{
+		"jay-babu/mason-null-ls.nvim",
+		-- overrides `require("mason-null-ls").setup(...)`
+		opts = function(_, opts)
+			-- add more things to the ensure_installed table protecting against community packs modifying it
+			opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+				"prettier",
+				"stylua",
+				-- add more arguments for adding more null-ls sources
+			})
+		end,
+	},
+	{
+		"jay-babu/mason-nvim-dap.nvim",
+		-- overrides `require("mason-nvim-dap").setup(...)`
+		opts = function(_, opts)
+			-- add more things to the ensure_installed table protecting against community packs modifying it
+			opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+				"python",
+				-- add more arguments for adding more debuggers
+			})
+		end,
+	},
 }
