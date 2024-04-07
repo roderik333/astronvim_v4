fa4d90fd99003004cfe80a611360f2634b2df940
Rune H. Steinnes <rune.steinnes@westum.no>
2024-04-07 22:34:36 +0200

    Git gutter

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
--- AstroUI provides the basis for configuring the AstroNvim User Interface
+---- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
 --       as this provides autocomplete and documentation while editing
