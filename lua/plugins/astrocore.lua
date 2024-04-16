12b31ae71b64575122f4f2bfdb23ed95eed10483
Rune H Steinnes <rune.steinnes@renroros.no>
2024-04-16 21:35:36 +0200

    Added Trouble

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -107,6 +107,20 @@ return {
 				-- 	"<cmd>CopilotChatReset<cr>",
 				-- 	desc = "CopilotChat - Reset chat history and clear buffer",
 				-- },
+
+				["<Leader>x"] = { desc = "Trouble(dev branch)" },
+				["<Leader>xx"] = { "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+				["<Leader>xX"] = {
+					"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+					desc = "Buffer Diagnostics (Trouble)",
+				},
+				["<Leader>xs"] = { "<cmd>Trouble symbols toggle focus=false<cr>", desc = "Symbols (Trouble)" },
+				["<Leader>xl"] = {
+					"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+					desc = "LSP Definitions / references / ... (Trouble)",
+				},
+				["<Leader>xL"] = { "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+				["<Leader>xQ"] = { "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
 			},
 			t = {
 				-- setting a mapping to false will disable it
---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,8 @@
+return {
+	{
+		"folke/trouble.nvim",
+		branch = "dev", -- IMPORTANT!
+		-- keys are handled i astrocore.lua
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+	},
+}
