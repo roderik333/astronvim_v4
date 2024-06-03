2c322f2ff90869cb249eb2111e3132f63b0cc662
Rune Hansén Steinnes <rune.steinnes@westum.no>
2024-06-03 09:30:25 +0200

    Removed dev branch

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,7 +1,6 @@
 return {
 	{
 		"folke/trouble.nvim",
-		branch = "dev", -- IMPORTANT!
 		-- keys are handled i astrocore.lua
 		opts = {}, -- for default options, refer to the configuration section for custom setup.
 	},
